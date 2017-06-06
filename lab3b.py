201ead30c9e84276dc0038f24038f73b3a6827b2
NileshCGupta <ngup@g.ucla.edu>
2017-06-05 23:15:57 -0700

    block consistency in progress

--- lab3b.py
+++ lab3b.py
@@ -17,7 +17,8 @@ actuallinkscount = {}
 # datablocks = {inode, [[datablocks], given links count, actual links count]}
 
 superblock = f.readline().split(',')
-i_first_free = superblock[7]
+num_blocks = int(superblock[1])
+i_first_free = int(superblock[7])
 
 for line in f:
 	if line.startswith("BFREE"):
@@ -31,7 +32,11 @@ for line in f:
 		inode = curline[1]
 		linkscount = curline[6]
 		i_datablocks = curline[12:24]
-		datablocks[inode] = i_datablocks
+		datablocks[inode] = []
+		for i in range(0, 12):
+			tup = (curline[12+i], '0')
+			datablocks[inode].append(tup)
+
 		givenlinkscount[inode] = linkscount
 		if int(inode) < int(i_first_free):
 			reservedblocks.extend(i_datablocks)
@@ -59,5 +64,40 @@ for line in f:
 		tup = (blocknum, level)
 		datablocks[inode].append(tup)
 
-# print(actuallinkscount)
+while '0' in reservedblocks:
+	reservedblocks.remove('0')
+
+for key in datablocks:
+	while ('0', '0') in datablocks[key]:
+		datablocks[key].remove(('0', '0'))
+
+level = { 	0: "", 
+			1: "INDIRECT", 
+			2: "DOUBLE INDIRECT", 
+			3: "TRIPLE INDIRECT" }
+			
+all_datablocks = []
+
+for key in datablocks:
+	for value in datablocks[key]:
+		#print(int(value))
+		if int(value[0]) > num_blocks:
+			print("INVALID ", level[int(value[1])], " BLOCK ", value, " IN INODE ", key, " AT OFFSET ", datablocks[key].index(value), "\n")
+
+		if int(key) > i_first_free:
+			if int(value[0]) < i_first_free:
+				print("RESERVED ", level[int(value[1])], " BLOCK ", value, " IN INODE ", key, " AT OFFSET ", datablocks[key].index(value), "\n")
+
+		if value[0] in bfree:
+			print("ALLOCATED ", level[int(value[1])], " BLOCK ", value, " ON FREELIST\n")
+
+		if value[0] in all_datablocks:
+			print("DUPLICATE ", level[int(value[1])], " BLOCK IN INODE ", value, " AT OFFSET ", datablocks[key].index(value), "\n")
+		else:
+			all_datablocks.append(int(value[0]))
+
+print(datablocks)
+
+
+
 f.close()
