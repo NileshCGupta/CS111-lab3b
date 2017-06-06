cb72fd50d62c9ffec4b100667634a427f44dd7a7
Christopher Thai <chris98.vthai@sbcglobal.net>
2017-06-05 23:17:43 -0700

    Revert "Added some debugging output"
    This reverts commit 21c6ed846126c92385ad64c8310d8492de7409ff.

--- lab3b.py
+++ lab3b.py
@@ -7,8 +7,7 @@ f = open(filename, 'r')
 
 bfree = []
 ifree = []
-inodes = [] #List of all inodes
-directoryInodes = [] #List of all DIRECTORY inodes
+inodes = []
 reservedblocks = []
 datablocks = {}
 dirents = {}
@@ -30,29 +29,24 @@ for line in f:
 	if line.startswith("INODE"):
 		curline = line.split(',')
 		inode = curline[1]
-		inodes.append(inode)
 		linkscount = curline[6]
 		i_datablocks = curline[12:24]
-		#print(i_datablocks)
 		datablocks[inode] = i_datablocks
 		givenlinkscount[inode] = linkscount
 		if int(inode) < int(i_first_free):
 			reservedblocks.extend(i_datablocks)
-			#directoryInodes.append(inode) 
-		if curline[2] == 'd':
-			directoryInodes.append(inode)
 
 	if line.startswith("DIRENT"):
 		curline = line.split(',')
 		inode = curline[1]
 		i_dirent = curline[3]
-		# add entry to directory data 
+		# add entry to directory data
 		if inode in dirents:
 			dirents[inode].append(i_dirent)
 		else:
 			dirents[inode] = [i_dirent]
 		# add reference count for entry
-		if i_dirent in actuallinkscount: 
+		if i_dirent in actuallinkscount:
 			actuallinkscount[i_dirent] += 1
 		else:
 			actuallinkscount[i_dirent] = 1
@@ -65,46 +59,5 @@ for line in f:
 		tup = (blocknum, level)
 		datablocks[inode].append(tup)
 
-#To remove 0 data blocks that may occur in reserved blocks
-while '0' in reservedblocks: 
-	reservedblocks.remove('0')
-
-for x in inodes:
-	while '0' in datablocks[x]:
-		datablocks[x].remove('0')
-
-
 # print(actuallinkscount)
-#Some debugging information to make sure we have all the data we need
-
-#Free blocks
-for x in bfree:
-	print("Free block: ", x)
-print('\n')
-
-#Free inodes
-for x in ifree:
-	print("Free inode: ", x)
-print('\n')
-
-#Reserved data blocks
-for x in reservedblocks:
-	print("Reserved block: ", x)
-print('\n')
-
-for x in inodes:
-	print("Inode number: ", x)
-	print("Inode uses these data blocks: ", datablocks[x])
-	print("Inode ", x, " supposedly has ", givenlinkscount[x], "links in filesystem")
-	print("Inode ", x, " actually has ", actuallinkscount[x], "links in filesystem")
-	print("\n")
-
-for x in directoryInodes:
-	print("Directory inode number: ", x)
-	print("Directory inode", x, " uses these data blocks: ", datablocks[x])
-	print("Directory inode", x, " has entries pointing to these inodes: ", dirents[x])
-	print("\n")
-
-
-
 f.close()
